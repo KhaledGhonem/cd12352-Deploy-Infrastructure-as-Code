c731746732d8b1d1a237e66d0816e346b5c6339f
KhaledGhonem <kghonem5@gmail.com>
2025-08-07 16:32:00 +0300

    commit

--- networking-and-infrastructure/create.sh
+++ networking-and-infrastructure/create.sh
@@ -1,4 +1,4 @@
-aws cloudformation create-stack --stack-name udacity-workflow-lesson \
-    --template-body file://workflow-and-helpers.yml   \
-    --parameters file://workflow-and-helpers-parameters.json  \
+aws cloudformation create-stack --stack-name $1 \
+    --template-body file://$2   \
+    --parameters file://$3  \
     --region=us-east-1
--- networking-and-infrastructure/update.sh
+++ networking-and-infrastructure/update.sh
@@ -1,4 +1,4 @@
-aws cloudformation update-stack --stack-name udacity-workflow-lesson \
-    --template-body file://workflow-and-helpers.yml   \
-    --parameters file://workflow-and-helpers-parameters.json  \
+aws cloudformation update-stack --stack-name $1\
+    --template-body file://$2   \
+    --parameters file://$3  \
     --region=us-east-1
